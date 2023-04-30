fb0b4be264fd3549ba2e858c2ddfed4477f7e4ac
David Demerchyan <davidemer11@yandex.ru>
2023-04-30 14:02:36 +0300

    alpha v.1

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
-###### [ [eng](Telegram-bot-for-employees-birthdays) / [rus](Телеграм-бот) ]
+[ [eng](#Telegram-bot-for-employees-birthdays) / 
+  [rus](#Телеграм-бот-для-дней-рождения-сотрудников) ]
 
-# Telegram bot for employees birthdays
+# Telegram-bot for employees' birthdays
+
+- Bot notifies employees in the chat about the upcoming birthday of a colleague
+- Bot registers the contribution for congratulations
+- Saves data to the database
+- Clears the chat history
+- Adds a new employee to the database
+- Removes a former employee from the database
+
+
+_______________________________________________________________________________
+# Телеграм-бот для дней рождения сотрудников
+
+- Бот уведомляет сотрудников в чате о предстоящем дне рождении коллеги
+- Бот регистрирует сумму взноса на поздравление
+- Сохраняет данные в базу данных
+- Очищает историю чата
+- Добавляет нового сотрудника в БД
+- Удаляет бывшего сотрудника из БД
+
+### Установка:
+
+- Добавить в своего бота команды /help, /add, /table
+- Добавить в корневую папку файл .env, внести в него
---
+++ main/main.py
@@ -0,0 +1,172 @@
+import datetime
+import os
+import sqlite3
+import threading
+import time
+
+import schedule
+from dotenv import load_dotenv
+from telebot import TeleBot
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+NOTIFY_TO = os.getenv('NOTIFY_TO')
+
+con = sqlite3.connect('../database.db', check_same_thread=False)
+cur = con.cursor()
+
+
+bot = TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(
+        message.chat.id,
+        'Привет, я телеграм-бот. Помогаю нашим сотрудникам не забывать '
+        'про дни рождения друг друга. Меня нужно добавить в чат и рассказать '
+        'мне о всех работниках. \n\n'
+        'Если нужна будет помощь вызывай команду /help.'
+    )
+
+
+@bot.message_handler(commands=['add'])
+def add_info(message):
+    bot.send_message(
+        message.chat.id,
+        'Чтобы добавить нового сотрудника нужно просто написать мне его '
+        'данные в правильном порядке и виде. Напиши мне ключевое слово '
+        '"Добавить" и далее '
+        'данные через один пробел. Если число однозначное не надо добавлять 0 в '
+        'начало. Снизу пример: \n\n'
+        'Добавить Майкл Скотт @telegram_mike 15 Март'
+    )
+
+
+@bot.message_handler(regexp='^Д|добавить')
+def add_employee(message):
+    months = {
+        'январь': 1,
+        'февраль': 2,
+        'март': 3,
+        'апрель': 4,
+        'май': 5,
+        'июнь': 6,
+        'июль': 7,
+        'август': 8,
+        'сентябрь': 9,
+        'октябрь': 10,
+        'ноябрь': 11,
+        'декабрь': 12,
+    }
+
+    data = message.text.split()
+
+    if data[5].lower() not in months:
+        bot.send_message(message.chat.id, 'Проверь, пожалуйста, месяц и '
+                                          'попробуй заново.')
+    else:
+        cur.executescript(f'''
+            CREATE TABLE IF NOT EXISTS birthdays(
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                surname TEXT,
+                tg_name TEXT,
+                day INTEGER,
+                month INTEGER
+            );
+
+            INSERT INTO birthdays(name, surname, tg_name, day, month)
+            VALUES ('{data[1]}', '{data[2]}', '{data[3]}',
+                    {int(data[4])}, {months[data[5].lower()]});
+        ''')
+        con.commit()
+        bot.send_message(message.chat.id, 'Я добавил в таблицу. Посмотреть ее '
+                                          'можно вызовом /table')
+
+
+@bot.message_handler(commands=['delete'])
+def delete_info(message):
+    bot.send_message(
+        message.chat.id,
+        'Чтобы удалить сотрудника из списка необходимо написать ключевое '
+        'слово "Удалить" и далее ID сотрудника. \n\n'
+        'Удалить 6'
+    )
+
+
+@bot.message_handler(regexp='^У|удалить')
+def delete_employee(message):
+    data = message.text.split()
+    employee = cur.execute(f'''
+        SELECT name, surname
+        FROM birthdays
+        WHERE id = {int(data[1])};
+    ''')
+
+    for i in employee:
+        employee = i
+    print(employee)
+    cur.execute(f'''
+        DELETE FROM birthdays
+        WHERE id = {int(data[1])};
+    ''')
+    con.commit()
+    bot.send_message(message.chat.id,
+                     f'Удалил сотрудника {" ".join(employee)}')
+
+
+@bot.message_handler(commands=['table'])
+def send_table(message):
+    table = cur.execute(f'''
+        SELECT *
+        FROM birthdays
+        ORDER BY surname;
+    ''')
+    result = 'ID Имя Фамилия Телеграм Число Месяц \n\n'
+    for employee in table:
+        for data in employee:
+            result += str(data) + ' '
+        result += '\n'
+    bot.send_message(message.chat.id, result)
+
+
+# def notify():
+#     today = datetime.date.today()
+#     day = int(str(today).split('-')[2])
+#     month = int(str(today).split('-')[1])
+#
+#     cur.execute(f'''
+#         SELECT name, surname, tg_name
+#         FROM birthdays
+#         WHERE day = {day + 1} AND month = {month};
+#     ''')
+#
+#     for x in cur:
+#         print(x)
+
+
+def notify2():
+    sec_now = int(time.strftime('%S'))
+    cur.execute(f'''
+            SELECT sec, surname
+            FROM birthdays
+            WHERE sec = {sec_now + 10};
+        ''')
+    for res in cur:
+        if res:
+            bot.send_message(NOTIFY_TO, f'Через 10 сек ДР у {res[1]}')
+
+
+def notifier():
+    schedule.every(1).second.do(notify2)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    notifier = threading.Thread(target=notifier)
+    notifier.start()
+    bot.polling()
